c73d82b772a096fb100492589a44237b871d8126
Iris Buschelman <ikatbuschel@gmail.com>
2018-04-25 17:12:59 -0700

    Skeleton Code

--- Source1.cpp
+++ Source1.cpp
@@ -1,2 +1,20 @@
 // USC Knots Project but in C this time
+#include flint2
+
+void swDetq(int q[], int m[], int nCrs) {}
+
+void swDetq_k(int s, int mLength) {}
+
+void rbRep(int q[], int braid[][], int vars[] /*???*/) {}
+
+int[] sSubMatrices(int m[]) {}
+
+int[] braidMonomialHelper(int ncPart[], int ncrs[], int signs[]) {}
+
+int[] bmhc(int bmhLocalMonomial[], int nCrs[], int signs[])[]
+
+int duplicateReduction(int nCrs[], numColors) {
+	return 0;
+}
+
 
